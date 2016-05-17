d7a6e90dfe499431e4b2c460bede8a7ddcf00981
Asparuh Vitkinov <Asparuh Vitkinov>
2016-05-17 22:34:28 +0300

    Init project

---
+++ src/main/java/bg/vitkinov/edu/SmileServiceApplication.java
@@ -0,0 +1,12 @@
+package bg.vitkinov.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileServiceApplication.class, args);
+	}
+}
